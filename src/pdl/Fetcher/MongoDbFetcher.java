5d8b2cbd8000dbaa49570a4edda3492777378114
Unknown <rboitard@gmail.com>
2017-11-13 14:13:35 +0100

    fetcher
    add fetcher filles

---
+++ src/pdl/Fetcher/MongoDbFetcher.java
@@ -0,0 +1,14 @@
+package pdl.Fetcher;
+
+public class  MongoDbFetcher implements IFetcher {
+
+    @Override
+    public void getProducts(String keywords, String filter) {
+
+    }
+
+    @Override
+    public void prettify() {
+
+    }
+}
