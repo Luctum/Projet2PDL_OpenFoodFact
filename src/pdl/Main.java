0b427c4991e3feeaa1966039ee672544048ac26b
Abraham Mubanzo <ab.learn.code@gmail.com>
2017-11-20 11:30:28 +0100

    added opencsv in pom.

--- src/pdl/Main.java
+++ src/pdl/Main.java
@@ -1,11 +1,12 @@
 package pdl;
 
 import pdl.Controller.AppController;
+import java.io.IOException;
 
 
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
           AppController.run();
     }
 
