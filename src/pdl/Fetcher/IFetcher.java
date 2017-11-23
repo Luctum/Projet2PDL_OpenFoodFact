8d52efa39a53cfd256b17776415fe5fbf56357c1
Unknown <rboitard@gmail.com>
2017-11-23 11:53:52 +0100

    Update getProducts
    return products list

--- src/pdl/Fetcher/IFetcher.java
+++ src/pdl/Fetcher/IFetcher.java
@@ -2,7 +2,9 @@ package pdl.Fetcher;
 
 import pdl.Model.Config;
 
+import java.util.List;
+
 public interface IFetcher {
-    public String getProducts();
+    public List<String> getProducts();
     public void prettify();
 }
