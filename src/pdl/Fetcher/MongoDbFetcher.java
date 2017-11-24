de03bc9e7cf2db9662e33f786f3d35ed0047b7c8
Unknown <rboitard@gmail.com>
2017-11-24 21:25:16 +0100

    add methode on MongoDbFetcher
    methode prettify and getField

--- src/pdl/Fetcher/MongoDbFetcher.java
+++ src/pdl/Fetcher/MongoDbFetcher.java
@@ -1,10 +1,14 @@
 package pdl.Fetcher;
 
+import org.json.JSONObject;
 import pdl.Model.Config;
 import com.google.gson.JsonObject;
 import com.mongodb.*;
+import pdl.Model.Product;
+
 import java.net.UnknownHostException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 
@@ -13,23 +17,20 @@ public class MongoDbFetcher implements IFetcher {
     private MongoClient mongoClient ;
     private DBCollection dbCollection;
     private Config config;
+    private List<Product> listProduct;
 
     public MongoDbFetcher( Config config, String dbName)
     {
        this.config = config;
-        try {
-            this.mongoClient = new MongoClient("localhost" , config.getMongoPort() );
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
-        }
-
+        this.mongoClient = new MongoClient("localhost" , config.getMongoPort() );
         DB database = this.mongoClient.getDB(dbName);
-        dbCollection = database.getCollection("products");
+        dbCollection = database.getCollection(config.getCollectionName());
+        listProduct = new ArrayList<Product>();
     }
 
     @Override
     public List<String> getProducts() {
-
+        System.out.println("Executing getProducts");
         List<String>products = new ArrayList<>();
         DBObject query =  new BasicDBObject();
         query.put(this.config.getFieldToSearch(), new BasicDBObject("$in", this.config.getSearchWords()));
@@ -37,11 +38,53 @@ public class MongoDbFetcher implements IFetcher {
         while(DBcursor.hasNext()) {
             products.add(DBcursor.next()+"");
         }
+        System.out.println("Executing getProducts done");
         return products;
     }
 
     @Override
     public void prettify() {
+        System.out.println("Executing prettify");
+        List<String> listStr = this.getProducts();
+        Iterator<String> it = listStr.iterator();
+        while(it.hasNext()){
+            Product product = new Product();
+            JSONObject json = json = new JSONObject(it.next());
+            product.setName(this.getField(json,"product_name"));
+            product.setNutritionGrade(this.getField(json,"nutrition_grades"));
+            json = (JSONObject)json.get("nutriments");
+            product.addNutriment("sugars_100g", this.getField(json,"sugars_100g"));
+            product.addNutriment("fiber_100g",this.getField(json,"fiber_100g"));
+            product.addNutriment("sodium_100g",this.getField(json,"sodium_100g"));
+            product.addNutriment("carbohydrates_100g",this.getField(json,"carbohydrates_100g"));
+            product.addNutriment("fat_100g",this.getField(json,"fat_100g"));
+            product.addNutriment("salt_100g",this.getField(json,"salt_100g"));
+            product.addNutriment("proteins_100g",this.getField(json,"proteins_100g"));
+            product.addNutriment("saturated_fat_100g", this.getField(json,"saturated_fat_100g"));
+            product.addNutriment("energy_100g", this.getField(json,"energy_100g"));
+            this.listProduct.add(product);
+        }
+        System.out.println("Executing prettify done");
+    }
 
+    public String getField(JSONObject json, String fieldName){
+        String str = "";
+        try{
+            str = json.get(fieldName).toString();
+        }
+        catch(org.json.JSONException e){
+            System.out.println(fieldName + ": not found");
+            str = "";
+        }
+        return str;
+    }
+
+
+    public List<Product> getListProduct() {
+        return listProduct;
+    }
+
+    public void setListProduct(List<Product> listProduct) {
+        this.listProduct = listProduct;
     }
 }
