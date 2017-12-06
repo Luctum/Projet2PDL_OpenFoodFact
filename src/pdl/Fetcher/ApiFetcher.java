719081a19e738a060d5a1eb98d70397100512bab
Abraham Mubanzo <ab.learn.code@gmail.com>
2017-12-06 13:07:09 +0100

    created API Fetcher to collect information from open food facts API.

---
+++ src/pdl/Fetcher/ApiFetcher.java
@@ -0,0 +1,28 @@
+package pdl.Fetcher;
+
+import java.util.List;
+
+/**
+ * Fetches information from Open Food Facts API
+ * @author : Abraham Mubanzo
+ * @author : Marlène Akimana
+ */
+public class ApiFetcher implements IFetcher {
+
+    /**
+     *
+     * @return
+     */
+    @Override
+    public List<String> getProducts() {
+        return null;
+    }
+
+    /**
+     *
+     */
+    @Override
+    public void prettify() {
+
+    }
+}
