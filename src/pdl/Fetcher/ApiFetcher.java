3323af530aeb75109721cd689c5ea117fca7a153
Abraham Mubanzo <ab.learn.code@gmail.com>
2017-12-07 15:29:35 +0100

    Supplied ApiFetcher with additional methods.

--- src/pdl/Fetcher/ApiFetcher.java
+++ src/pdl/Fetcher/ApiFetcher.java
@@ -8,10 +8,31 @@ import java.util.List;
  * @author : Marlène Akimana
  */
 public class ApiFetcher implements IFetcher {
+    /**
+     * base URL to make calls on the API for product information by its name
+     */
+    private String searchUrlByProductName;
+    /**
+     * base URL to make calls on the API for product information by its barcode
+     */
+    private String searchUrlByProductByCode;
+    /**
+     * base URL to make calls on the API for category information
+     */
+    private String searchUrlByCategory;
 
     /**
-     *
-     * @return
+     * Preferred Constructor
+     * Instantiates {@param searchUrlByProductName}, {@param searchUrlByProductByCode}, {@param searchUrlByCategory}
+     */
+    public ApiFetcher(){
+        this.searchUrlByCategory = "https://ssl-api.openfoodfacts.org/category/";
+        this.searchUrlByProductByCode = "https://ssl-api.openfoodfacts.org/api/vO/product/";
+        this.searchUrlByProductName = "https://ssl-api.openfoodfacts.org/code/";
+    }
+    /**
+     * Container of the JSON data we received from the API
+     * @return {@link List} object containing {@link String} objects
      */
     @Override
     public List<String> getProducts() {
@@ -24,5 +45,52 @@ public class ApiFetcher implements IFetcher {
     @Override
     public void prettify() {
 
+    }
+
+    /**
+     * Creates a url to send to the API to search for product information by its name
+     * @param keyword product name
+     */
+    public void setSearch(String keyword){
+    }
+
+    /**
+     * Creates an URL to send to the API to search for product information by its bar code
+     * @param code
+     */
+    public void setSearchByCode(String code){
+
+    }
+
+    /**
+     * Creates a url to send to the API to search for categories information
+     * @param category
+     */
+    public void setSearchByCategory(String category){
+
+    }
+
+    /**
+     *
+     * @return search url by product name
+     */
+    public String getSearchUrlByProductName() {
+        return this.searchUrlByProductName;
+    }
+
+    /**
+     *
+     * @return search url by product code
+     */
+    public String getSearchUrlByProductByCode() {
+        return this.searchUrlByProductByCode;
+    }
+
+    /**
+     *
+     * @return search url by category
+     */
+    public String getSearchUrlByCategory() {
+        return this.searchUrlByCategory;
     }
 }
