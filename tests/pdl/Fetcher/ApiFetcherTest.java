e41e6f0e9054511ee8ffdbee077b5c6849993214
Abraham Mubanzo <ab.learn.code@gmail.com>
2017-12-07 15:49:59 +0100

    Added ApiFetcher Test class.

---
+++ tests/pdl/Fetcher/ApiFetcherTest.java
@@ -0,0 +1,49 @@
+package pdl.Fetcher;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ApiFetcherTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void getProducts() throws Exception {
+    }
+
+    @Test
+    public void prettify() throws Exception {
+    }
+
+    @Test
+    public void setSearch() throws Exception {
+    }
+
+    @Test
+    public void setSearchByCode() throws Exception {
+    }
+
+    @Test
+    public void setSearchByCategory() throws Exception {
+    }
+
+    @Test
+    public void getSearchUrlByProductName() throws Exception {
+    }
+
+    @Test
+    public void getSearchUrlByProductByCode() throws Exception {
+    }
+
+    @Test
+    public void getSearchUrlByCategory() throws Exception {
+    }
+
+    @Test
+    public void getClient() throws Exception {
+    }
+
+}
