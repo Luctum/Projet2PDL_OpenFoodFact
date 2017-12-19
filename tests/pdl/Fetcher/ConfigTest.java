33ab1bb1fdc0d21c1e2ea9e09cd08efbf2ca64dc
Unknown <hugo.palla@gmail.com>
2017-12-19 16:00:53 +0100

    Config test update can't check for an Exception correctly...

--- tests/pdl/Fetcher/ConfigTest.java
+++ tests/pdl/Fetcher/ConfigTest.java
@@ -1,6 +1,5 @@
 package pdl.Fetcher;
 
-import okhttp3.ResponseBody;
 import org.junit.Before;
 import org.junit.Test;
 import pdl.Model.Config;
@@ -12,56 +11,14 @@ import static org.junit.Assert.*;
  * {@link ApiFetcherTest} Test class
  */
 public class ConfigTest {
-
-    Config c1;
-    Config c2;
-    Config c3;
-    Config c4;
-    Config c5;
-
-    @Before
-    public void setUp() throws Exception {
-        c2 = ConfigReader.readConfig("config2.json");
-        c3 = ConfigReader.readConfig("config3.json");
-        c4 = ConfigReader.readConfig("config5.json");
-        c5 = ConfigReader.readConfig("config5.json");
-    }
-
     /**
-     * Test a good config
+     * Test if the config file is imported correctly
      * @throws Exception
      */
-    @Test(expected = Exception.class)
+    @Test
     public void configTestGoodConfig() throws Exception {
-        c1 = ConfigReader.readConfig("config.json");
+        Config c1 = ConfigReader.readConfig("config.json");
         assertNotNull(c1);
     }
 
-    /**
-     * Test a good config
-     * @throws Exception
-     */
-    @Test
-    public void configTestBadConfig() throws Exception {
-        c2 = ConfigReader.readConfig("config.json");
-        assertNotNull(c1);
-    }
-
-    /**
-     * Test a good config
-     * @throws Exception
-     */
-    @Test
-    public void configTestBadConfig2() throws Exception {
-        c2 = ConfigReader.readConfig("config.json");
-    }
-
-    /**
-     * Test a good config
-     * @throws Exception
-     */
-    @Test
-    public void configTestBadConfig3() throws Exception {
-        //assertNotNull();
-    }
 }
